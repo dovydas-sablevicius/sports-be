8507f9856b9087fab9b579c71cfd9e9007dc025a
Dovydas Šablevičius <dovydas.sablevicius.it@gmail.com>
2024-11-19 14:46:09 +0200

    5 add tests

--- src/main/java/com/sourcery/sport/match/dto/MatchDto.java
+++ src/main/java/com/sourcery/sport/match/dto/MatchDto.java
@@ -2,12 +2,14 @@ package com.sourcery.sport.match.dto;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class MatchDto {
   private String id;
   private String nextMatchId;
--- src/main/java/com/sourcery/sport/match/dto/MatchPlayerDto.java
+++ src/main/java/com/sourcery/sport/match/dto/MatchPlayerDto.java
@@ -1,12 +1,14 @@
 package com.sourcery.sport.match.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class MatchPlayerDto {
   private String id;
   private Integer score;
